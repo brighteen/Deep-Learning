11b815cbe193fabbfd435b32acb77687ba955e92
여명구 <88411212+brighteen@users.noreply.github.com>
2025-03-11 19:47:53 +0900

    chicken dataset

---
+++ Object_Detection/detect_video.py
@@ -0,0 +1,26 @@
+import cv2
+from ultralytics import YOLO
+
+# 모델 로드
+model = YOLO("yolov8s.pt")
+
+# 비디오 파일 열기
+cap = cv2.VideoCapture("datas/닭축사.mp4")
+
+while cap.isOpened():
+    ret, frame = cap.read()
+    if not ret:
+        break
+
+    # 각 프레임에 대해 예측 수행
+    results = model.predict(frame, save=True)
+
+    # 결과 시각화 (프레임에 박스 그리기)
+    annotated_frame = results[0].plot()
+
+    cv2.imshow("Detection", annotated_frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
--- Object_Detection/detect_yolov8s.py
+++ Object_Detection/detect_yolov8s.py
@@ -4,7 +4,7 @@ import cv2
 model = YOLO("yolov8s.pt") # 원하는 크기 모델 입력(n ~ x)
 
 # result = model.predict("./test4.jpg", save=False, conf=0.5) # conf=0.5 : 신뢰도가 0.5 이상인 것만 출력
-result = model.predict("test4.jpg")
+result = model.predict("datas/test4.jpg")
 plots = result[0].plot()
 
 # 박스 정보 확인(좌표, 신뢰도, 클래스)
