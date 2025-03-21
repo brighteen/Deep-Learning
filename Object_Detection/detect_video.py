fe12350a06a3692b7b293f830ce3cdab38f5a3bb
여명구 <88411212+brighteen@users.noreply.github.com>
2025-03-21 13:36:40 +0900

    ViT

--- Object_Detection/detect_video.py
+++ Object_Detection/detect_video.py
@@ -2,13 +2,10 @@ import cv2
 from ultralytics import YOLO
 
 # 모델 로드
-# model = YOLO("yolov8s.pt")
 model = YOLO("best_chick.pt")
 
 # 비디오 파일 열기
-# cap = cv2.VideoCapture("datas/chicken_farm.mp4")
-# cap = cv2.VideoCapture("datas/0_8_IPC1_20220912080906.mp4")
-cap = cv2.VideoCapture("left_half.mp4")
+cap = cv2.VideoCapture(r"C:\Users\brigh\Documents\GitHub\Deep-Learning\Object_Detection\datas\0_8_IPC1_20230106081037.mp4")
 
 while cap.isOpened():
     ret, frame = cap.read()
@@ -16,7 +13,7 @@ while cap.isOpened():
         break
 
     # 각 프레임에 대해 예측 수행
-    results = model.predict(frame)
+    results = model.predict(frame, imgsz=1280, conf=0.3, iou=0.5)
 
     # 결과 시각화 (프레임에 박스 그리기)
     annotated_frame = results[0].plot()
@@ -25,7 +22,6 @@ while cap.isOpened():
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-print(len(results))
 
 cap.release()
 cv2.destroyAllWindows()
