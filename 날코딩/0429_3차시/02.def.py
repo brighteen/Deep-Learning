b3ac14a55a45ad626937792202b2ba127111bad4
여명구 <88411212+brighteen@users.noreply.github.com>
2025-04-29 18:59:48 +0900

    중간수정

--- 날코딩/0429_3차시/02.def.py
+++ 날코딩/0429_3차시/02.def.py
@@ -4,8 +4,22 @@ def add(x=0, y=10): # defalut x=0, y=10
     y = x + y
     return y
 
-print(f'함수 호출: {add(x=1,y=2)}') # x=1, y=2
-print(f'함수 호출: {add(2)}') # x=2, y=10
+# print(f'함수 호출: {add(x=1,y=2)}') # x=1, y=2
+# print(f'함수 호출: {add(2)}') # x=2, y=10
 
-print(type(add)) # <class 'function'>
-print(type(add(1,2))) # <class 'int'>
+# print(type(add)) # <class 'function'>
+# print(type(add(1,2))) # <class 'int'>
+
+# class 선언
+class 사람:
+    def __init__(self, 이름):
+        self.이름 = 이름
+
+    def 인사(self):
+        # return print(f'안녕하세요, {self}입니다.')
+        print('안녕하세요')
+        # print(f'안녕하세요, {self.이름}입니다.')
+    
+h1 = 사람(이름='여명구')
+print(h1.이름) # 여명구
+h1.인사()
