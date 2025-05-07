708be1eade108912bed7ca16dc15ce2cf05a2a0b
여명구 <88411212+brighteen@users.noreply.github.com>
2025-05-07 15:59:56 +0900

    수업 전 예습

--- 날코딩/0507_5차시/00. 수업 전.py
+++ 날코딩/0507_5차시/00. 수업 전.py
@@ -15,7 +15,41 @@ else:
 for i in [1,2,3]:
     print(f"{i}번째 반복")
 
-def hello(object1):
+# 함수 정의
+def hello(object1): # object1이라는 인수 지정
     print("hello world " + object1 + "!")
 
 hello("yaay")
+
+print("---\n")
+
+class Man:
+    def __init__(self, name):
+        self.name = name
+        print("Initialized!")
+    
+    def hello(self):
+        print("Hello " + self.name + "!")
+
+    def goodbye(self):
+        print("Good bye " + self.name + "!")
+
+m = Man("David")
+m.hello()
+m.goodbye()
+
+import numpy as np
+
+x = np.array([[1,2], [3,4]])
+print(f"\nx_shape: {x.shape}")
+print(f"x/20: \n{x / 2.0}")
+
+y = np.array([[3,0], [0,6]])
+print(f"\nx + y: \n{x+y}")
+
+# 브로드캐스트
+print("\n브로드캐스트: 2x2에 스칼라(10)를 곱하면 10으로 이루어진 2x2행렬를 곱한 연산이 됨.")
+print(f"x * 10: \n {x*10}")
+
+y = np.array([10,20])
+print(f"x*y: \n{x*y}")
