18b43c6716024269f3e1ecbf002422ab6e3030b8
여명구 <88411212+brighteen@users.noreply.github.com>
2025-05-07 16:19:48 +0900

    3차시 리뷰

--- 날코딩/0430_4차시/02.3차시_시험리뷰(신경망).py
+++ 날코딩/0430_4차시/02.3차시_시험리뷰(신경망).py
@@ -1,13 +1,16 @@
 import numpy as np
 
-'''def 내적(a, b):
+'''
+def 내적(a, b):
     return np.dot(a, b)
 a = 1
 b =2 
 c = 3
 print(f'a + b = {내적(1, 2)+ 3}')
 print(np.dot(a, b)+ c)
-print(f'{a} * {b} + {c} = {a*b + c}')'''
+print(f'{a} * {b} + {c} = {a*b + c}')
+'''
+
 np.random.seed(0)
 
 class LinearModel:
@@ -16,6 +19,7 @@ class LinearModel:
         self.b = np.zeros(1)
         self.x = None
         self.y = None
+        print("Initialize!")
         print(f'weight w: {self.w}, w.shape: {self.w.shape}')
         print(f'bios b: {self.b}, b.shape: {self.b.shape}')
 
@@ -24,6 +28,8 @@ class LinearModel:
         self.y = np.dot(self.w,x) + self.b
         # print(f'input x: {x}')
         # print(f'pred y: {y}, y.shape: {y.shape}')
+        print("\nforward")
+        print(f'input: {model.x}, pred: {self.y}')
         return self.y
     
     def backward(self, dout): # dout :아직 모르는 loss의 미분값
@@ -31,6 +37,8 @@ class LinearModel:
         dx = np.dot(dout, self.w) # dL_dx = dL_dy * dy_dx
         dw = np.dot(dout, self.x) # dL_dw = dL_dy * dy_dw
         db = np.dot(dout, 1) # dL_db = dL_dy * dy_db
+        print("\nbackward")
+
         return dx, dw, db # dx: x에 대한 미분값, dw: w에 대한 미분값, db: b에 대한 미분값
 
 
@@ -39,9 +47,8 @@ model = LinearModel() # 모델 선언
 # print(f'w: {model.w}')
 # print(f'b: {model.b}')
 y = model.forward(x)
-
 # print(x) # 전역변수 x를 출력
-print(f'\ninput: {model.x}, pred: {y}') # model 안에 x를 출력
+# print(f'\ninput: {model.x}, pred: {y}') # model 안에 x를 출력
 
 dout = 1
 grad_x, grad_w, grad_b = model.backward(dout)
