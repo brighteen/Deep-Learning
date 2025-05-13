c596186778962d2634df4a7db00488607f3caf8a
여명구 <88411212+brighteen@users.noreply.github.com>
2025-05-13 21:57:18 +0900

    6차시 일단 끝

--- 날코딩/0513_6차시/main.py
+++ 날코딩/0513_6차시/main.py
@@ -14,22 +14,31 @@ if __name__ == "__main__": # 다른 파일에서 main.py를 가져올 때 이 __
     model2 = LinearModel()
     mse = MeanSquardError()
 
-    print(f"[model1] w1: {model1.w}, b1: {model1.b}")
-    print(f"[model2] w2: {model2.w}, b2: {model2.b}")
+    for i in range(0,3):
+        print(f'\n===={i+1}번째 반복====')
+        print(f"[model1] w1: {model1.w}, b1: {model1.b}")
+        print(f"[model2] w2: {model2.w}, b2: {model2.b}")
 
-    # forward
-    out = model1.forward(x)
-    print(f"y1[w1 * x + b1]: {out}")
-    out = model2.forward(out)
-    print(f"y2[w2 * y1 + b2]: {out}")
-    out = mse.forward(out, t)
-    print(f"loss[(y2 - t)^2]: {out}")
+        # forward
+        out = model1.forward(x) # out = w1*x+b1
+        print(f"y1: {out}")
+        out = model2.forward(out) # out = w2*y1+b2
+        print(f"y2: {out}")
+        out = mse.forward(out, t) # out = (y2-t)^2
+        print(f"loss: {out}")
 
-    # backward
-    dout = 1
-    dout = mse.backward(dout=dout) # dL/dy2
-    print(f"[dL/dy2]: {dout}")
-    dw2, db2, dx2 = model2.backward(dout) # dx2 = dL/dy1
-    print(f"[dL/dw2]: {dw2}, [dL/db2]: {db2}, [dL/dy1]: {dx2}")
-    dw1, db1, dx1 = model1.backward(dx2)
-    print(f"[dL/dw1]: {dw1}, [db1]: {db1}, [dx1]: {dx1}")
+        # backward
+        dout = 1
+        dout = mse.backward(dout=dout) # dout = dL/dy2
+        print(f"dy2]: {dout}")
+        dw2, db2, dout = model2.backward(dout) # dout = dL/dy1
+        print(f"[dL/dw2]: {dw2}, [dL/db2]: {db2}, [dL/dy1]: {dout}")
+        dw1, db1, dout = model1.backward(dout) # dout = dL/dx
+        print(f"[dL/dw1]: {dw1}, [dL/db1]: {db1}, [dL/dx]: {dout}")
+
+        # update, gradient descent(하강)
+        lr = 0.01
+        model2.w -= np.dot(lr, dw2)
+        model2.b -= np.dot(lr, db2)
+        model1.w -= np.dot(lr, dw1)
+        model1.b -= np.dot(lr, db1)
